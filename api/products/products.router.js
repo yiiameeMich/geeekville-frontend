9776a72d2f36a02223c43b8eb6a297b7bbb94e3c
yiiameeMich <michaelkukh.dev@gmail.com>
2023-05-09 12:10:34 +0300

    Change project structure. Divide into two repos - backend and frontend

--- api/products/products.router.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const controller = require('./products.controller')
-
-router.post('/create-product', controller.createProduct)
-
-router.get('/all-products', controller.getAllProducts)
-
-module.exports = router
